3a2f0ae69945280344481a466ad33065ccb2d4db
david-asem <ddy.asem@gmail.com>
2021-10-13 01:10:47 +0000

    added methods for delete, put and post

--- photonHTTP Lib/app.js
+++ photonHTTP Lib/app.js
@@ -1,3 +1,27 @@
 const http = new PhotonHTTP();
 
-http.get('https://jsonplaceholder.typicode.com/users').then(data=> console.log(data)).catch(err => console.log(err))
+//get a user
+// http.get('https://jsonplaceholder.typicode.com/users').then(data=> console.log(data)).catch(err => console.log(err))
+
+//user data
+const data = {
+  name: 'David Asem',
+  username: 'davidass',
+  email:'davidass@johndoe.com'
+}
+
+// //create a user
+// http.post('https://jsonplaceholder.typicode.com/users', data)
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
+
+  //update a user
+// http.put('https://jsonplaceholder.typicode.com/users/1', data)
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
+
+
+//delete user
+  http.delete('https://jsonplaceholder.typicode.com/users/1')
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
